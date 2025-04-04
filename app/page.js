39de274bca3a2629f7d1e940ed957a93df2c71c0
Josejose424 <132400358+Josejose424@users.noreply.github.com>
2025-04-03 22:16:48 -0500

    Mobile now :)

--- app/page.js
+++ app/page.js
@@ -10,10 +10,10 @@ import MouseAvoidGame from './components/MouseAvoidGame';
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-neutral-100'>
       {/* Desktop / Bento Box Layout (visible on medium and larger screens) */}
       <div className="hidden md:block">
-        <div className="bg-white">
+        <div className="bg-neutral-100">
           <section className="wave-section">
             <div className="wave">
               <span></span>
@@ -21,7 +21,7 @@ export default function Home() {
               <span></span>
             </div>
             {/* White background section */}
-            <div className="grid items-center justify-items-center h-[750px] bg-white">
+            <div className="grid items-center justify-items-center h-[750px] bg-neutral-100">
               <main className="z-10 grid grid-cols-6 grid-rows-4 gap-4 w-[980px] max-w-7xl p-4 pt-8 h-[700px] bg-neutral-800 rounded-3xl">
                 <div className="col-span-2 row-span-2 bg-neutral-white rounded-3xl flex items-center justify-center text-white text-lg holographic-card">
                   <img src="/csnow.png" alt="selfie" className="" />
@@ -75,37 +75,85 @@ export default function Home() {
           </section>
         </div>
       </div>
+      
 
       {/* Mobile Layout (visible on small screens) */}
-<div className="block flex flex-col md:hidden">
-  <div className="bg-white p-4">
-    {/* Welcome */}
-    <div className="block md:hidden p-4">
-      <div className="bg-neutral-800 rounded-3xl text-white p-4 flex flex-col items-center justify-center">
-        <div className="text-center leading-tight">
-          <h1 className="text-2xl font-bold mb-2">Welcome!</h1>
-          <p className="text-base">To my Personal Website</p>
-        </div>
-        <div className="h-[60px] w-full flex items-center justify-center">
-          <FluidString />
-        </div>
-      </div>
-    </div>
+        <div className="block flex flex-col md:hidden">
+          <div className="bg-neutral-100 p-4">
+            
+            {/* Welcome */}
+            <div className="block md:hidden p-4">
+              <div className="bg-neutral-800 rounded-3xl text-white p-4 flex flex-col items-center justify-center">
+                <div className="text-center leading-tight">
+                  <h1 className="text-2xl font-bold mb-2">Welcome!</h1>
+                  <p className="text-base">To my Personal Website</p>
+                </div>
+                <div className="h-[60px] w-full flex items-center justify-center">
+                  <FluidString />
+                </div>
+              </div>
+            </div>
 
-    {/* About Me */}
-    <div className="block md:hidden p-4">
-      <div className="bg-neutral-800 rounded-3xl text-white p-4 flex flex-col items-center justify-center">
-        <h1 className="text-2xl font-bold mb-2">About Me</h1>
-        <Typewriter text="Hi! I'm Jose Nunez — a proud Mexican-American Software Developer with a passion for Mobile Development, UI/UX Design, and Project Management. Outside of tech, I love playing soccer, rock climbing, volleyball, and recently started running!" />
-      </div>
-    </div>
-  </div>
-</div>
+            {/* About Me */}
+            <div className="block md:hidden p-4">
+              <div className="bg-neutral-800 rounded-3xl text-white p-4 flex flex-col items-center justify-center">
+                <h1 className="text-2xl font-bold mb-2">About Me</h1>
+                <Typewriter text="Hi! I'm Jose Nunez — a proud Mexican-American Software Developer with a passion for Mobile Development, UI/UX Design, and Project Management." />
+              </div>
+            </div>
+            
+            {/* Selfie Card for Mobile */}
+                <div className="block md:hidden p-4">
+                  <div className="bg-neutral-white h-[300px] overflow-hidden rounded-3xl flex items-center justify-center text-white text-lg p-4">
+                      <img src="/csnow.png" alt="selfie" className="w-full rounded-3xl h-full object-cover"/>
+                  </div>
+                </div>
+
+            {/* Mouse Avoid Game for Mobile */}
+            <div className="block md:hidden p-4">
+            <div className="relative bg-neutral-800 h-full rounded-3xl flex items-center justify-center text-white text-lg">   
+                <MouseAvoidGame />
+              </div>
+            </div> 
+
+
+            {/* Let's Connect Card for Mobile */}
+            <div className="block md:hidden p-4">
+              <div className="bg-neutral-800 rounded-3xl flex flex-col items-center justify-center text-white text-lg p-4">
+                <p className="text-lg mb-4">Lets Connect!</p>
+                <ul className="flex space-x-4">
+                  <li>
+                    <a href="#" className="social-icon">
+                      <i className="fab fa-facebook-f icon" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="social-icon">
+                      <i className="fab fa-twitter icon" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.linkedin.com/in/jose-n132/" className="social-icon">
+                      <i className="fab fa-linkedin-in icon" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.instagram.com/_jose.hal/" className="social-icon">
+                      <i className="fab fa-instagram icon" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        
 
     
 
       {/* Rest of your content remains unchanged */}
-      <div className="bg-white px-4 sm:px-8">
+      <div className="bg-neutral-100 px-4 sm:px-8">
         <h1 className="text-4xl font-bold flex items-center justify-center p-4 mt-8 mb-4 text-black"> Projects </h1>
         {/* Black background section */}
         <div className="grid grid-cols-2 grid-rows-2 gap-4 w-full max-w-7xl mx-auto p-4 mt-4 h-[500px] bg-neutral-800 rounded-3xl">
@@ -128,70 +176,87 @@ export default function Home() {
       <h1 className="text-4xl font-bold flex items-center justify-center p-4 mt-8 mb-4 text-black"> About Me </h1>
       
       
-      <div className="w-[1000px] mx-auto bg-white rounded-3xl p-4">
-        <Swiper
-          modules={[Navigation, Pagination, Autoplay]}
-          spaceBetween={30}
-          slidesPerView={4}
-          loop={true}
-          autoplay={{
-            delay: 4000,
-            disableOnInteraction: false,
-          }}
-          className="my-8"
-        >
-          <SwiperSlide>
-            <div className="w-[200px] h-auto aspect-[9/16] bg-neutral-900 rounded-2xl overflow-hidden">
-              <img src="/Favs/Boat.jpeg" alt="Project 1" className="w-full h-full object-cover" />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="w-[200px] h-auto aspect-[9/16] bg-neutral-900 rounded-2xl overflow-hidden">
-              <img src="/Favs/Temple.jpeg" alt="Project 1" className="w-full h-full object-cover" />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="w-[200px] h-auto aspect-[9/16] bg-neutral-900 rounded-2xl overflow-hidden">
-              <img src="/Favs/Colors.jpeg" alt="Project 1" className="w-full h-full object-cover" />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="w-[200px] h-auto aspect-[9/16] bg-neutral-900 rounded-2xl overflow-hidden">
-              <img src="/Favs/Greece.jpeg" alt="Project 1" className="h-full w-auto object-cover" />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="w-[200px] h-auto aspect-[9/16] bg-neutral-900 rounded-2xl overflow-hidden">
-              <img src="/Favs/Hawaii.jpeg" alt="Project 1" className="h-full w-auto object-cover" />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="w-[200px] h-auto aspect-[9/16] bg-neutral-900 rounded-2xl overflow-hidden">
-              <img src="/Favs/Camel.jpeg" alt="Project 1" className="w-full h-full object-cover" />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="w-[200px] h-auto aspect-[9/16] bg-neutral-900 rounded-2xl overflow-hidden">
-              <img src="/Favs/Petra.jpeg" alt="Project 1" className="w-full h-full object-cover" />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="w-[200px] h-auto aspect-[9/16] bg-neutral-900 rounded-2xl overflow-hidden">
-              <img src="/Favs/Temple.jpeg" alt="Project 1" className="w-full h-full object-cover" />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="w-[200px] h-auto aspect-[9/16] bg-neutral-900 rounded-2xl overflow-hidden">
-              <img src="/Favs/Tut.jpeg" alt="Project 1" className="w-full h-full object-cover" />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="w-[200px] h-auto aspect-[9/16] bg-neutral-900 rounded-2xl overflow-hidden">
-              <img src="/Favs/Tokyo.jpeg" alt="Project 1" className="w-full h-full object-cover" />
-            </div>
-          </SwiperSlide>
-        </Swiper>
+      <div className="w-full max-w-[1000px] mx-auto bg-white rounded-3xl p-4">
+  <Swiper
+    modules={[Navigation, Pagination, Autoplay]}
+    loop={true}
+    autoplay={{
+      delay: 4000,
+      disableOnInteraction: false,
+    }}
+    spaceBetween={30}
+    breakpoints={{
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+    }}
+    className="my-8"
+  >
+    <SwiperSlide>
+      <div className="h-auto aspect-[9/16] bg-neutral-900 rounded-2xl overflow-hidden">
+        <img src="/Favs/Boat.jpeg" alt="Project 1" className="w-full h-full object-cover" />
       </div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div className="h-auto aspect-[9/16] bg-neutral-900 rounded-2xl overflow-hidden">
+        <img src="/Favs/Temple.jpeg" alt="Project 1" className="w-full h-full object-cover" />
+      </div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div className="h-auto aspect-[9/16] bg-neutral-900 rounded-2xl overflow-hidden">
+        <img src="/Favs/Colors.jpeg" alt="Project 1" className="w-full h-full object-cover" />
+      </div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div className="h-auto aspect-[9/16] bg-neutral-900 rounded-2xl overflow-hidden">
+        <img src="/Favs/Greece.jpeg" alt="Project 1" className="h-full w-auto object-cover" />
+      </div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div className="h-auto aspect-[9/16] bg-neutral-900 rounded-2xl overflow-hidden">
+        <img src="/Favs/Hawaii.jpeg" alt="Project 1" className="h-full w-auto object-cover" />
+      </div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div className="h-auto aspect-[9/16] bg-neutral-900 rounded-2xl overflow-hidden">
+        <img src="/Favs/Camel.jpeg" alt="Project 1" className="w-full h-full object-cover" />
+      </div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div className="h-auto aspect-[9/16] bg-neutral-900 rounded-2xl overflow-hidden">
+        <img src="/Favs/Petra.jpeg" alt="Project 1" className="w-full h-full object-cover" />
+      </div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div className="h-auto aspect-[9/16] bg-neutral-900 rounded-2xl overflow-hidden">
+        <img src="/Favs/Temple.jpeg" alt="Project 1" className="w-full h-full object-cover" />
+      </div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div className="h-auto aspect-[9/16] bg-neutral-900 rounded-2xl overflow-hidden">
+        <img src="/Favs/Tut.jpeg" alt="Project 1" className="w-full h-full object-cover" />
+      </div>
+    </SwiperSlide>
+    <SwiperSlide>
+      <div className="h-auto aspect-[9/16] bg-neutral-900 rounded-2xl overflow-hidden">
+        <img src="/Favs/Tokyo.jpeg" alt="Project 1" className="w-full h-full object-cover" />
+      </div>
+    </SwiperSlide>
+  </Swiper>
+</div>
     </div>
   );
 }
